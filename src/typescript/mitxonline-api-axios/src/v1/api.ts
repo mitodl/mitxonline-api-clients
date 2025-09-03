0302b130909591ec3fb5b09592d40e610ce6c972
MIT Open Learning Engineering <odl-devops@mit.edu>
2025-09-03 12:07:22 +0000

    Generated clients for mitxonline from rev: 0d8f10e257919d4bcebdd5f114b4590f1aab8cb8

--- src/typescript/mitxonline-api-axios/src/v1/api.ts
+++ src/typescript/mitxonline-api-axios/src/v1/api.ts
@@ -9705,6 +9705,103 @@ export class UserSearchApi extends BaseAPI {
 
 
 /**
+ * UserinfoApi - axios parameter creator
+ * @export
+ */
+export const UserinfoApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * Retrieve the current user\'s info only if they have an edx_username, otherwise return 409  This is to prevent issues with Open edX OAuth client that expect an edx_username to be present
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        userinfoRetrieve: async (options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            const localVarPath = `/api/v0/userinfo/`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * UserinfoApi - functional programming interface
+ * @export
+ */
+export const UserinfoApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = UserinfoApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * Retrieve the current user\'s info only if they have an edx_username, otherwise return 409  This is to prevent issues with Open edX OAuth client that expect an edx_username to be present
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async userinfoRetrieve(options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<User>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.userinfoRetrieve(options);
+            const index = configuration?.serverIndex ?? 0;
+            const operationBasePath = operationServerMap['UserinfoApi.userinfoRetrieve']?.[index]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, operationBasePath || basePath);
+        },
+    }
+};
+
+/**
+ * UserinfoApi - factory interface
+ * @export
+ */
+export const UserinfoApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = UserinfoApiFp(configuration)
+    return {
+        /**
+         * Retrieve the current user\'s info only if they have an edx_username, otherwise return 409  This is to prevent issues with Open edX OAuth client that expect an edx_username to be present
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        userinfoRetrieve(options?: RawAxiosRequestConfig): AxiosPromise<User> {
+            return localVarFp.userinfoRetrieve(options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * UserinfoApi - object-oriented interface
+ * @export
+ * @class UserinfoApi
+ * @extends {BaseAPI}
+ */
+export class UserinfoApi extends BaseAPI {
+    /**
+     * Retrieve the current user\'s info only if they have an edx_username, otherwise return 409  This is to prevent issues with Open edX OAuth client that expect an edx_username to be present
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof UserinfoApi
+     */
+    public userinfoRetrieve(options?: RawAxiosRequestConfig) {
+        return UserinfoApiFp(this.configuration).userinfoRetrieve(options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+
+/**
  * UsersApi - axios parameter creator
  * @export
  */
